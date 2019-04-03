809c89e06f447cd24ab6d39c67b91c135fe21c42
Shangyangyang <602390269@qq.com>
2019-04-03 14:39:36 +0800

    增加了esay poi 的 excel导出

--- src/main/java/cn/ainannan/tool/cost/bean/Car.java
+++ src/main/java/cn/ainannan/tool/cost/bean/Car.java
@@ -1,17 +1,22 @@
 package cn.ainannan.tool.cost.bean;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import cn.ainannan.base.bean.BaseBean;
 
 public class Car extends BaseBean {
+	@Excel(name = "金额", orderNum="1")
 	private Double money;
+	@Excel(name = "类型", orderNum="2", replace= {"油费_1", "过路费_2", "罚款_3", "保险_4", "其它_5"})
 	private String type;
 	private String typeName;
+	@Excel(name = "里程数", orderNum="3")
 	private Integer licheng;
+	@Excel(name = "油价", orderNum="4")
 	private Double youjia;
+	@Excel(name = "说明", orderNum="6")
 	private String reason;
+	@Excel(name = "日期", orderNum="5")
 	private String inputDate;
-
-	
 	
 	public Car() {
 		super();
--- src/main/java/cn/ainannan/tool/cost/controller/CarController.java
+++ src/main/java/cn/ainannan/tool/cost/controller/CarController.java
@@ -3,6 +3,7 @@ package cn.ainannan.tool.cost.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import cn.ainannan.base.result.ResultGen;
 import cn.ainannan.base.result.ResultObject;
 import cn.ainannan.commons.utils.DateUtil;
 import cn.ainannan.commons.utils.NumberUtils;
+import cn.ainannan.excel.EasyPoiUtil;
 import cn.ainannan.tool.cost.bean.Car;
 import cn.ainannan.tool.cost.bean.CarOilWear;
 import cn.ainannan.tool.cost.service.CarService;
@@ -25,20 +27,29 @@ import cn.ainannan.tool.cost.service.CarService;
 public class CarController {
 	@Autowired
 	private CarService carService;
-	
-	@RequestMapping({"","list"})
-	public ResultObject list(Car car, @RequestParam(defaultValue = "1") Integer page, 
+
+	@RequestMapping({ "", "list" })
+	public ResultObject list(Car car, @RequestParam(defaultValue = "1") Integer page,
 			@RequestParam(defaultValue = "20") Integer size, HttpServletRequest request) {
 		PageHelper.startPage(page, size);
-		
+
 		List<Car> list = carService.findList(car);
 		PageInfo<Car> pageInfo = new PageInfo<Car>(list);
-		
+
 		return ResultGen.genSuccessResult(pageInfo);
 	}
-	
+
+	@RequestMapping("export")
+	public void export(Car car, HttpServletResponse response) {
+
+		List<Car> list = carService.findList(car);
+		// 导出操作
+		EasyPoiUtil.exportExcel(list, "汽车花费", "total", Car.class, "汽车花费.xls", response);
+	}
+
 	/**
 	 * 保存
+	 * 
 	 * @param car
 	 * @return
 	 */
@@ -47,62 +58,62 @@ public class CarController {
 		carService.save(car);
 		return ResultGen.genSuccessResult();
 	}
-	
+
 	/**
 	 * 删除
+	 * 
 	 * @param car
 	 * @return
 	 */
 	@RequestMapping("delete")
 	public ResultObject delete(String id) {
-		
+
 		carService.delete(new Car(id));
 		return ResultGen.genSuccessResult();
 	}
-	
-	@RequestMapping(value="getOilWear")
-	public ResultObject getOilWear(Car car){
-		car.setType("1"); 	// 油费
+
+	@RequestMapping(value = "getOilWear")
+	public ResultObject getOilWear(Car car) {
+		car.setType("1"); // 油费
 		CarOilWear cow = null;
-		
+
 		List<Car> carList = carService.findList(car);
-		
-		if(carList.size() > 0){
-			int min = carList.get(0).getLicheng();			
+
+		if (carList.size() > 0) {
+			int min = carList.get(0).getLicheng();
 			int max = carList.get(0).getLicheng();
 			double money = 0D;
 			double shengTotal = 0D;
 			String startTime = carList.get(0).getInputDate();
 			String endTime = carList.get(0).getInputDate();
-			
+
 			for (Car c : carList) {
-				
-				if(c.getLicheng() < min){
+
+				if (c.getLicheng() < min) {
 					min = c.getLicheng();
 					startTime = c.getInputDate();
 				}
-				if(c.getLicheng() > max){
+				if (c.getLicheng() > max) {
 					max = c.getLicheng();
 					endTime = c.getInputDate();
 				}
-				
+
 				money += c.getMoney();
 				shengTotal += c.getMoney() / c.getYoujia();
 			}
-			
+
 			// 计算
 			int lichengTotal = max - min;
-			
+
 			double baigongliyouhao = shengTotal / lichengTotal * 100;
-						
-			cow = new CarOilWear(NumberUtils.getSubDouble(shengTotal), DateUtil.getDate(startTime), 
+
+			cow = new CarOilWear(NumberUtils.getSubDouble(shengTotal), DateUtil.getDate(startTime),
 					DateUtil.getDate(endTime), lichengTotal, NumberUtils.getSubDouble(money),
 					NumberUtils.getSubDouble(money / shengTotal), NumberUtils.getSubDouble(baigongliyouhao));
-			
-			
+
 		}
-		
+
 		return ResultGen.genSuccessResult(cow);
 	}
-	
+
 }
