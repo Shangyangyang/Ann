47dbd7733cde83084f0b71cba555811a1b7892c0
Shangyangyang <602390269@qq.com>
2021-04-22 16:04:05 +0800

    增加异星工厂游戏存档

---
+++ src/main/java/cn/ainannan/base/bean/Tongji.java
@@ -0,0 +1,24 @@
+package cn.ainannan.base.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Tongji{
+	private String niandu;
+	private String s1;
+	private String s2;
+	private Integer n1;
+	private Integer n2;
+	private Integer n3;
+	private Double m1;
+	private Double m2;
+	private Double m3;
+	private Double m4;
+
+}
